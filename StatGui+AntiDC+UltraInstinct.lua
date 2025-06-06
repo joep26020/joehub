7586e0b0c4fe969b2a546d06e67ffb0c4990509b
joep26020 <143763241+joep26020@users.noreply.github.com>
2025-06-05 23:34:42 -0500

    Update StatGui+AntiDC+UltraInstinct.lua

--- StatGui+AntiDC+UltraInstinct.lua
+++ StatGui+AntiDC+UltraInstinct.lua
@@ -412,7 +412,7 @@ local function updateGuiLift()
 	    local innerScale = h.evasiveBar.inner.Size.X.Scale
 	    local baseT = (1 - innerScale) * 0.8
 	    h.evasiveBar.glow.ImageTransparency = math.max(t, baseT)
-	    h.evasiveBar.glow.Visible = (innerScale < 1)
+	    h.evasiveBar.glow.Visible = true
 	end
     end
 end
